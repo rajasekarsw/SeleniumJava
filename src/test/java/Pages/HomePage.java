241a6dd722e0a4025f4c00b4a667ebaaba67e423
rajasekars <rajrams104@gmail.com>
2024-02-11 12:25:40 +0530

    pass commit

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -9,7 +9,7 @@ public class HomePage {
 
 
 
-    @FindBy(xpath="//i/parent::span[@class='oxd-userdropdown-ta']")
+    @FindBy(xpath="//i/parent::span[@class='oxd-userdropdown-tab']")
     private WebElement profileDropdownIcon;
 
     @FindBy(xpath="//i/parent::span[@class='oxd-userdropdown-tab']/following-sibling::ul/child::li/a[text()='Logout']")
